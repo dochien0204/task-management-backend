0370daca928c27b9511f00ca4999bbf257b867bb
docong2708 <dxcong2708@gmail.com>
2024-05-08 23:41:59 +0700

    feat: add document for task

---
+++ infrastructure/repository/task_document.go
@@ -0,0 +1,39 @@
+package repository
+
+import (
+	"log"
+	"source-base-go/entity"
+
+	"gorm.io/gorm"
+)
+
+type TaskDocumentRepository struct {
+	db *gorm.DB
+}
+
+func NewTaskDocumentRepository(db *gorm.DB) *TaskDocumentRepository {
+	return &TaskDocumentRepository{
+		db: db,
+	}
+}
+
+func (r TaskDocumentRepository) WithTrx(trxHandle *gorm.DB) TaskDocumentRepository {
+	if trxHandle == nil {
+		log.Print("Transaction DB not found")
+		return r
+	}
+
+	r.db = trxHandle
+	return r
+}
+
+func (r TaskDocumentRepository) CreateDocumentsForTask(listData []*entity.TaskDocument) error {
+	err := r.db.Model(&entity.TaskDocument{}).
+		Create(&listData).Error
+	
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- usecase/task/service.go
+++ usecase/task/service.go
@@ -1,6 +1,7 @@
 package task
 
 import (
+	"fmt"
 	payload "source-base-go/api/payload/task"
 	taskPayload "source-base-go/api/payload/task"
 	"source-base-go/config"
@@ -14,17 +15,20 @@ import (
 type Service struct {
 	taskRepo TaskRepository
 	statusRepo StatusRepository
+	taskDocumentRepo TaskDocumentRepository
 }
 
-func NewService(taskRepo TaskRepository, statusRepo StatusRepository) *Service {
+func NewService(taskRepo TaskRepository, statusRepo StatusRepository, taskDocumentRepo TaskDocumentRepository) *Service {
 	return &Service{
 		taskRepo: taskRepo,
 		statusRepo: statusRepo,
+		taskDocumentRepo: taskDocumentRepo,
 	}
 }
 
 func (s Service) WithTrx(trxHandle *gorm.DB) Service {
 	s.taskRepo = s.taskRepo.WithTrx(trxHandle)
+	s.taskDocumentRepo = s.taskDocumentRepo.WithTrx(trxHandle)
 
 	return s
 }
@@ -58,6 +62,26 @@ func (s Service) CreateTask(userId int, payload taskPayload.TaskPayload) error {
 	if err != nil {
 		return err
 	}
+
+	//Create task document
+	listTaskDocument := []*entity.TaskDocument{}
+	for _, documentPayload := range payload.Documents {
+		path := fmt.Sprintf("task/%d/%v", data.Id, documentPayload.File)
+		fileName := fmt.Sprintf("https://%v.s3.%v.amazon.com/%v", config.S3_BUCKET_NAME, config.REGION, path)
+		document := &entity.TaskDocument{
+			File: fileName,
+			Name: documentPayload.Name,
+			TaskId: data.Id,
+		}
+
+		listTaskDocument = append(listTaskDocument, document)
+	}
+
+	err = s.taskDocumentRepo.CreateDocumentsForTask(listTaskDocument)
+	if err != nil {
+		return err
+	}
+	
 	return nil
 }
 
