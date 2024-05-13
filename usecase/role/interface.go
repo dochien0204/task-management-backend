f555068901e76e4a5f2afdb08b02a694e8bfa9f2
docong2708 <dxcong2708@gmail.com>
2024-05-13 19:19:39 +0700

    feat: get list role by type

--- usecase/role/interface.go
+++ usecase/role/interface.go
@@ -5,9 +5,11 @@ import "source-base-go/entity"
 type RoleRepository interface {
 	GetAllRole() ([]*entity.Role, error)
 	FindByCode(code string, typeRole string) (*entity.Role, error)
+	FindAllRoleByType(typeRole string) ([]*entity.Role, error)
 }
 
 type UseCase interface {
 	GetAllRole() ([]*entity.Role, error)
 	FindByCode(code string, typeRole string) (*entity.Role, error)
+	FindAllRoleByType(typeRole string) ([]*entity.Role, error)
 }
--- usecase/role/service.go
+++ usecase/role/service.go
@@ -19,3 +19,7 @@ func (s Service) GetAllRole() ([]*entity.Role, error) {
 func (s Service) FindByCode(code string, typeRole string) (*entity.Role, error) {
 	return s.roleRepo.FindByCode(code, typeRole)
 }
+
+func (s Service) FindAllRoleByType(typeRole string) ([]*entity.Role, error) {
+	return s.roleRepo.FindAllRoleByType(typeRole)
+}
